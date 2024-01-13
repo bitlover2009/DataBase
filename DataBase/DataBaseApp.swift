ac786c34291a2e8ec01e138d6f64e14fc6541b9f
bitlover2009 <bitlover2009@gmail.com>
2024-01-12 19:00:36 -0600

    Initial Commit

---
+++ DataBase/DataBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataBaseApp.swift
+//  DataBase
+//
+//  Created by David Grau Beltrán  on 12/01/24.
+//
+
+import SwiftUI
+
+@main
+struct DataBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
